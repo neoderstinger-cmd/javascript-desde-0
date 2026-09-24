8faa55e9cb4dcbc71fe22eef9af12394d41ad6f1
omar <neoderstinger@gmail.com>
2026-09-23 22:16:31 -0500

    pruebas con nodejs y consola

--- 01-FUNDAMENTOS/hola_mundo.js
+++ 01-FUNDAMENTOS/hola_mundo.js
@@ -1,3 +1,32 @@
 console.log("Hola Mundo");
-document.write("Hola Mundo");
+// introduccion a comnentarios
+
+//variales
+
+let 
+a = 10, 
+b = 20, 
+c = 30,
+d = 'hola ',
+f = 'spiderman',
+x = a + b
+h = d + f;
+
+
+//console.log(a);  imprime en consola el valor de la variable a
+//console.log(b);
+//console.log(c);
+//console.log(d);
+//console.log(f);
+//console.log(x);
+//console.log(h);
+
+//console.log('a',a); imprime en consola el valor de la variable a con el texto 'a'
+
+//console.log('a',a,'b',b,'c',c,'d',d,'f',f,'x',x,'h',h); imprime en consola el valor de todas las variables con su respectivo texto
+//console.log({a, b, c, d, f, x, h}); imprime en consola el valor de todas las variables en un objeto
+
+//console.log(a); imprime en consola el valor de la variable a
+//console.warn(b); imprime en consola el valor de la variable b con un mensaje de advertencia
+//console.error(c); imprime en consola el valor de la variable c con un mensaje de error
 
